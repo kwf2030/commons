db3952b581b6960b4f61ff77191c34749966a315
WangFeng <kwf2030@163.com>
2019-01-06 01:32:08 +0800

    update

--- wechatbot/msg.go
+++ wechatbot/msg.go
@@ -10,45 +10,21 @@ const (
   // 自带表情是文本消息，Content字段内容为：[奸笑]，
   // emoji表情也是文本消息，Content字段内容为：<span class="emoji emoji1f633"></span>，
   // 如果连同文字和表情一起发送，Content字段内容是文字和表情直接是混在一起，
-  // 位置坐标也是文本消息，Content字段内容为：
-  // 雨花台区雨花西路(德安花园东):/cgi-bin/mmwebwx-bin/webwxgetpubliclinkimg?url=xxx&msgid=741398718084560243&pictype=location
+  // 位置坐标也是文本消息，Content字段内容为：雨花台区雨花西路(德安花园东):/cgi-bin/mmwebwx-bin/webwxgetpubliclinkimg?url=xxx&msgid=741398718084560243&pictype=location
   MsgText = 1
 
-  // 图片/照片消息，
-  // Content字段内容为XML，Content字段内容为：
-  // <?xml version="1.0"?>
-  // <msg>
-  // <img aeskey="" encryver="" cdnthumbaeskey="" cdnthumburl="" cdnthumblength=""
-  //   cdnthumbheight="" cdnthumbwidth="" cdnmidheight="" cdnmidwidth="" cdnhdheight=""
-  //   cdnhdwidth="" cdnmidimgurl="" length="" md5="" /><br/>
-  // </msg>
+  // 图片/照片消息
   MsgImage = 3
 
+  // 语音消息
   MsgVoice = 34
 
-  // 被添加好友待验证，Content内容为：
-  // <msg fromusername="kwf2030" encryptusername="v1_400be59c1cd145d71bcd4a389b68456833bfdba992d524a563494beed6def517@stranger"
-  // fromnickname="kwf2030" content="我是客户"  shortpy="KWF2030" imagestatus="3" scene="30"
-  // country="CN" province="Jiangsu" city="Nanjing" sign="" percard="1" sex="1" alias="" weibo=""
-  // weibonickname="" albumflag="0" albumstyle="0" albumbgimgid="" snsflag="17" snsbgimgid=""
-  // snsbgobjectid="0" mhash="21b5f7503b9728a74f69ffe2ac4a81b8"
-  // mfullhash="21b5f7503b9728a74f69ffe2ac4a81b8"
-  // bigheadimgurl="http://wx.qlogo.cn/mmhead/ver_1/as2mDdcIHonnibUkbSzmyAZ4eRPFv67M7IOLXhE4ULXQaRESLaNnLlsjHGvFuNXicnqYmxCXCZFjziaGQetfFyRhQ/0"
-  // smallheadimgurl="http://wx.qlogo.cn/mmhead/ver_1/as2mDdcIHonnibUkbSzmyAZ4eRPFv67M7IOLXhE4ULXQaRESLaNnLlsjHGvFuNXicnqYmxCXCZFjziaGQetfFyRhQ/96"
-  // ticket="v2_1604664f28c4e339b63f5299ef578d15350d9b02ee5b8137b0c568f5423fa5adfe843d9a7478dbf21395f26ae4567896f52e6cdd9f2971b81f06332c1f2c91bf@stranger"
-  // opcode="2" googlecontact="" qrticket="" chatroomusername="" sourceusername="" sourcenickname="">
-  // <brandlist count="0" ver="683212005"></brandlist>
-  // </msg>
+  // 被添加好友待验证
   MsgVerify = 37
 
   MsgFriendRecommend = 40
 
-  // 名片消息，Content字段内容为：
-  // <?xml version="1.0"?>
-  // <msg bigheadimgurl="http://xxx" smallheadimgurl="http://xxx" username="v1_xxx@stranger" nickname=""
-  // shortpy="" alias="" imagestatus="" scene="" province="" city="" sign="" sex="" certflag=""
-  // certinfo="" brandIconUrl="" brandHomeUrl="" brandSubscriptConfigUrl="" brandFlags=""
-  // regionCode="" antispamticket="v2_xxx@stranger" />
+  // 名片消息
   MsgCard = 42
 
   // 拍摄（视频消息）
@@ -60,19 +36,8 @@ const (
 
   MsgLocation = 48
 
-  // 公众号推送的链接，
-  // 发送的文件也是链接消息，
-  // 分享的链接（AppMsgType=1/3/5），
-  // 红包（AppMsgType=2001）,
-  // 收藏也是连接消息，
-  // 实时位置共享也是链接消息，Content字段内容为：
-  // <msg>
-  // <appmsg appid="" sdkver="0">
-  // <type>17</type>
-  // <title><![CDATA[我发起了位置共享]]></title>
-  // </appmsg>
-  // <fromusername>kwf2030</fromusername>
-  // </msg>
+  // 公众号推送的链接，分享的链接（AppMsgType=1/3/5），红包（AppMsgType=2001），
+  // 发送的文件，收藏，实时位置共享
   MsgLink = 49
 
   MsgVoip = 50
@@ -87,19 +52,10 @@ const (
   MsgNotice = 9999
 
   // 系统消息，
-  // 例如通过好友验证，系统会发送"你已添加了..."和"如果陌生人..."的消息，
-  // 例如"实时位置共享已结束"的消息
+  // 例如通过好友验证，系统会发送"你已添加了..."，"如果陌生人..."，"实时位置共享已结束"的消息，
   MsgSystem = 10000
 
-  // 撤回消息，Content字段内容为：
-  // <sysmsg type="revokemsg">
-  // <revokemsg>
-  // <session>Nickname</session>
-  // <oldmsgid>1057920614</oldmsgid>
-  // <msgid>2360839023010332147</msgid>
-  // <replacemsg><![CDATA["Nickname" 撤回了一条消息]]></replacemsg>
-  // </revokemsg>
-  // </sysmsg>
+  // 撤回消息
   MsgRevoke = 10002
 )
 
@@ -114,30 +70,33 @@ var (
   jsonPathUrl          = []string{"Url"}
 )
 
+type GroupMessage struct {
+  FromMemberUserName string
+  FromMemberContact  *Contact
+}
+
 type Message struct {
-  Content      string `json:"content,omitempty"`
-  CreateTime   int64  `json:"create_time"`
-  FromUserName string `json:"from_user_name,omitempty"`
-  ToUserName   string `json:"to_user_name,omitempty"`
-  Id           string `json:"id,omitempty"`
-  Type         int    `json:"type"`
-  Url          string `json:"url,omitempty"`
-  Raw          []byte `json:"raw,omitempty"`
+  Content      string
+  CreateTime   int64
+  FromUserName string
+  ToUserName   string
+  Id           string
+  Type         int
+  Url          string
+  Raw          []byte
 
-  // 如果是群聊，Speaker表示消息发送者
-  SpeakerUserName string   `json:"-"`
-  SpeakerContact  *Contact `json:"-"`
+  FromContact *Contact
+  ToContact   *Contact
+  Bot         *Bot
 
-  FromContact *Contact `json:"-"`
-  ToContact   *Contact `json:"-"`
-  Bot         *Bot     `json:"-"`
+  *GroupMessage
 }
 
 func buildMessage(data []byte) *Message {
   if len(data) == 0 {
     return nil
   }
-  ret := &Message{Raw: data}
+  ret := &Message{Raw: data, GroupMessage: &GroupMessage{}}
   jsonparser.EachKey(data, func(i int, v []byte, _ jsonparser.ValueType, e error) {
     if e != nil {
       return
@@ -170,6 +129,9 @@ func buildMessage(data []byte) *Message {
       ret.Url, _ = jsonparser.ParseString(v)
     }
   }, jsonPathContent, jsonPathCreateTime, jsonPathFromUserName, jsonPathToUserName, jsonPathMsgId, jsonPathNewMsgId, jsonPathMsgType, jsonPathUrl)
+  if len(ret.Content) >= 39 && ret.Content[33:34] == ":" {
+    ret.FromMemberUserName = ret.Content[1:33]
+  }
   return ret
 }
 
@@ -184,6 +146,9 @@ func (msg *Message) withBot(bot *Bot) {
     msg.ToContact = bot.Self
   } else if c := bot.Contacts.FindByUserName(msg.ToUserName); c != nil {
     msg.ToContact = c
+  }
+  if c := bot.Contacts.FindByUserName(msg.FromMemberUserName); c != nil {
+    msg.FromMemberContact = c
   }
   msg.Bot = bot
 }
