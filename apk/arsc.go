5733cd2d6db159124bf19e476b38df04898299b7
WangFeng <kwf2030@163.com>
2019-02-20 17:36:01 +0800

    update

---
+++ apk/arsc.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+  "io/ioutil"
+
+  "github.com/kwf2030/commons/conv"
+)
+
+// 8个字节
+type ResHeader struct {
+  // chunk类型
+  Type uint16
+
+  // chunk header大小
+  HeaderSize uint16
+
+  // chunk大小（header + data）
+  Size uint32
+}
+
+// 12个字节
+type ResTableHeader struct {
+  // 起始：0，
+  // 结束：8
+  ResHeader
+
+  // package资源包个数，通常一个app只有一个资源包，
+  // 起始：8，
+  // 结束：12
+  PackageCount uint32
+}
+
+// 28个字节
+type ResStrPoolHeader struct {
+  // 起始：12，
+  // 结束：20
+  ResHeader
+
+  // 字符串个数，
+  // 起始：20，
+  // 结束：24
+  StrCount uint32
+
+  // 字符串样式个数，
+  // 起始：24，
+  // 结束：28
+  StyleCount uint32
+
+  // 字符串标识，
+  // SortedFlag = 0x0001
+  // UTF16Flag = 0x0000
+  // UTF8Flag = 0x0100
+  // 起始：28，
+  // 结束：32
+  Flags uint32
+
+  // 字符串起始位置偏移（相对header），
+  // 起始：32，
+  // 结束：36
+  StrStart uint32
+
+  // 字符串样式起始位置偏移（相对header），
+  // 起始：36，
+  // 结束：40
+  StyleStart uint32
+}
+
+// 28+StrCount*4+StyleCount*4+字符串大小+字符串样式大小
+type ResStrPoolChunk struct {
+  // 起始：12，
+  // 结束：40
+  Header ResStrPoolHeader
+
+  // 字符串偏移数组，其元素对应Strs中每一个元素的起始位置，
+  // 长度为Header.StrCount，
+  // 起始：40，
+  // 结束：40+Header.StrCount*4
+  StrOffsets []uint32
+
+  // 字符串样式偏移数组，其元素对应Styles中每一个元素的起始位置，
+  // 长度为Header.StyleCount,
+  // 起始：40+Header.StrCount*4，
+  // 结束：40+Header.StrCount*4+Header.StyleCount*4
+  StyleOffsets []uint32
+
+  // 字符串，前两个字节为长度，
+  // 若是UTF-8编码，以0x00（1个字节）作为结束符，
+  // 若是UTF-16编码，以0x0000（2个字节）作为结束符，
+  // 起始：12+Header.StrStart，
+  // 结束：
+  Strs []string
+
+  // 字符串样式，
+  // 起始：12+Header.StyleStart，
+  // 结束：12+Header.Size
+  Styles []string
+}
+
+type ResArsc struct {
+  data []byte
+
+  TableHeader        ResTableHeader
+  GlobalStrPoolChunk ResStrPoolChunk
+}
+
+func ParseResArsc(file string) *ResArsc {
+  data, e := ioutil.ReadFile(file)
+  if e != nil {
+    return nil
+  }
+  ret := &ResArsc{data: data}
+  ret.parseTableHeader()
+  ret.parseGlobalStrPoolChunk()
+  return ret
+}
+
+func (r *ResArsc) parseTableHeader() {
+  r.TableHeader = ResTableHeader{
+    ResHeader:    ResHeader{conv.BytesToUint16L(r.data[:2]), conv.BytesToUint16L(r.data[2:4]), conv.BytesToUint32L(r.data[4:8])},
+    PackageCount: conv.BytesToUint32L(r.data[8:12]),
+  }
+}
+
+func (r *ResArsc) parseGlobalStrPoolChunk() {
+  header := ResStrPoolHeader{
+    ResHeader:  ResHeader{conv.BytesToUint16L(r.data[12:14]), conv.BytesToUint16L(r.data[14:16]), conv.BytesToUint32L(r.data[16:20])},
+    StrCount:   conv.BytesToUint32L(r.data[20:24]),
+    StyleCount: conv.BytesToUint32L(r.data[24:28]),
+    Flags:      conv.BytesToUint32L(r.data[28:32]),
+    StrStart:   conv.BytesToUint32L(r.data[32:36]),
+    StyleStart: conv.BytesToUint32L(r.data[36:40]),
+  }
+
+  var strOffsets []uint32
+  if header.StrCount > 0 {
+    strOffsets = make([]uint32, header.StrCount)
+    var s, e uint32
+    for i := uint32(0); i < header.StrCount; i++ {
+      s = 40 + i*4
+      e = s + 4
+      strOffsets[i] = conv.BytesToUint32L(r.data[s:e])
+    }
+  }
+
+  var styleOffsets []uint32
+  if header.StyleCount > 0 {
+    styleOffsets = make([]uint32, header.StyleCount)
+    var s, e uint32
+    for i := uint32(0); i < header.StyleCount; i++ {
+      s = 40 + i*4
+      e = s + 4
+      styleOffsets[i] = conv.BytesToUint32L(r.data[s:e])
+    }
+  }
+
+  var strs []string
+  if header.StrCount > 0 {
+    strs = make([]string, header.StrCount)
+    s := 12 + header.StrStart
+    var e uint32
+    if header.StyleCount > 0 {
+      e = 12 + header.StyleStart
+    } else {
+      e = 12 + header.Size
+    }
+    arr := r.data[s:e]
+    if header.Flags&0x0100 != 0 {
+      // UTF-8
+      for i := uint32(0); i < header.StrCount; i++ {
+        strs[i] = str8(arr, strOffsets[i])
+      }
+    } else {
+      // UTF-16
+      for i := uint32(0); i < header.StrCount; i++ {
+        strs[i] = str16(arr, strOffsets[i])
+      }
+    }
+  }
+
+  // todo parse style strings
+  r.GlobalStrPoolChunk = ResStrPoolChunk{
+    Header:       header,
+    StrOffsets:   strOffsets,
+    StyleOffsets: styleOffsets,
+    Strs:         strs,
+    Styles:       nil,
+  }
+}
+
+func str8(arr []byte, offset uint32) string {
+  n := 1
+  if x := arr[offset] & 0x80; x != 0 {
+    n = 2
+  }
+  s := offset + uint32(n)
+  l := arr[s]
+  if l == 0 {
+    return ""
+  }
+  s++
+  if l&0x80 != 0 {
+    l = (l&0x7F)<<8 | arr[s]&0xFF
+    s++
+  }
+  return string(arr[s : s+uint32(l)])
+}
+
+func str16(arr []byte, offset uint32) string {
+  n := 2
+  if x := arr[offset+1] & 0x80; x != 0 {
+    n = 4
+  }
+  s := offset + uint32(n)
+  e := s
+  l := uint32(len(arr))
+  for {
+    if e+1 >= l {
+      break
+    }
+    if arr[e] == 0 && arr[e+1] == 0 {
+      break
+    }
+    e += 2
+  }
+  return string(arr[s:e])
+}
--- apk/res.go
+++ apk/res.go
@@ -1,129 +1,14 @@
 package main
 
 import (
+  "errors"
   "fmt"
-  "io/ioutil"
-
-  "github.com/kwf2030/commons/conv"
+  "runtime"
 )
 
-// chunk类型
-const (
-  ResNull    = 0x0000
-  ResStrPool = 0x0001
-  ResTable   = 0x0002
-  ResXml     = 0x0003
-)
-
-// ResTable子类型
-const (
-  ResTablePackage     = 0x0200
-  ResTableType        = 0x0201
-  ResTableTypeSpec    = 0x0202
-  ResTableTypeLibrary = 0x0203
-)
-
-// ResXml子类型
-const (
-  ResXmlFirstChunk     = 0x0100
-  ResXmlStartNamespace = 0x0100
-  ResXmlEndNamespace   = 0x0101
-  ResXmlStartElement   = 0x0102
-  ResXmlEndElement     = 0x0103
-  ResXmlCData          = 0x0104
-  ResXmlLastChunk      = 0x017F
-  ResXmlResourceMap    = 0x0180
-)
-
-// 8个字节
-type ResHeader struct {
-  // chunk类型
-  Type uint16
-
-  // chunk header大小
-  HeaderSize uint16
-
-  // chunk大小（header + data）
-  Size uint32
-}
-
-// 12个字节
-type ResTableHeader struct {
-  // 起始：0，
-  // 结束：8
-  ResHeader
-
-  // package资源包个数，通常一个app只有一个资源包，
-  // 起始：8，
-  // 结束：12
-  PackageCount uint32
-}
-
-// 28个字节
-type ResStrPoolHeader struct {
-  // 起始：12，
-  // 结束：20
-  ResHeader
-
-  // 字符串个数，
-  // 起始：20，
-  // 结束：24
-  StrCount uint32
-
-  // 字符串样式个数，
-  // 起始：24，
-  // 结束：28
-  StyleCount uint32
-
-  // 字符串标识，
-  // SortedFlag = 1
-  // UTF16Flag = 0
-  // UTF8Flag = 1<<8(0x0100)
-  // 起始：28，
-  // 结束：32
-  Flags uint32
-
-  // 字符串起始位置偏移（相对header），
-  // 起始：32，
-  // 结束：36
-  StrStart uint32
-
-  // 字符串样式起始位置偏移（相对header），
-  // 起始：36，
-  // 结束：40
-  StyleStart uint32
-}
-
-// 28+StrCount*4+StyleCount*4+字符串大小+字符串样式大小
-type ResStrPoolChunk struct {
-  // 起始：12，
-  // 结束：40
-  Header ResStrPoolHeader
-
-  // 字符串偏移数组，其元素对应Strs中每一个元素的起始位置，
-  // 长度为Header.StrCount，
-  // 起始：40，
-  // 结束：40+Header.StrCount*4
-  StrOffsets []uint32
-
-  // 字符串样式偏移数组，其元素对应Styles中每一个元素的起始位置，
-  // 长度为Header.StyleCount,
-  // 起始：40+Header.StrCount*4，
-  // 结束：40+Header.StrCount*4+Header.StyleCount*4
-  StyleOffsets []uint32
-
-  // 字符串，前两个字节为长度：(((hbyte & 0x7F) << 8)) | lbyte
-  // 若是UTF-8编码，以0x00（1个字节）作为结束符，
-  // 若是UTF-16编码，以0x0000（2个字节）作为结束符，
-  // 与Styles一一对应，即如果Strs[n]有样式，那么其样式是Styles[n]，
-  // 起始：12+Header.StrStart，即StyleOffsets的结束位置，
-  // 结束：12+Header.StrStart+Header.StrCount*4，即12+Header.StyleStart
-  Strs []string
-
-  // 字符串样式，
-  // 起始：12+Header.StyleStart，即Strs的结束位置，
-  // 结束：12+Header.StyleStart+StyleCount*4，即12+Header.Size
-  Styles []string
+type ResStrStyle struct {
+  Ref   ResStrRef
+  Spans []ResStrSpan
 }
 
 type ResStrRef struct {
@@ -142,78 +27,17 @@ type ResStrSpan struct {
 }
 
 func main() {
-  // f, _ := ioutil.ReadFile("/home/wangfeng/workspace/wechat/tmp/resources.arsc")
-  f, _ := ioutil.ReadFile("C:\\Users\\WangFeng\\Desktop\\resources.arsc")
-
-  tableHeader := ResTableHeader{
-    ResHeader:    ResHeader{conv.BytesToUint16L(f[:2]), conv.BytesToUint16L(f[2:4]), conv.BytesToUint32L(f[4:8])},
-    PackageCount: conv.BytesToUint32L(f[8:12]),
+  var file string
+  switch runtime.GOOS {
+  case "windows":
+    file = "C:\\Users\\WangFeng\\Desktop\\resources.arsc"
+  case "linux":
+    file = "/home/wangfeng/workspace/wechat/tmp/resources.arsc"
+  default:
+    panic(errors.New("os not supported"))
   }
-
-  strPoolHeader := ResStrPoolHeader{
-    ResHeader:  ResHeader{conv.BytesToUint16L(f[12:14]), conv.BytesToUint16L(f[14:16]), conv.BytesToUint32L(f[16:20])},
-    StrCount:   conv.BytesToUint32L(f[20:24]),
-    StyleCount: conv.BytesToUint32L(f[24:28]),
-    Flags:      conv.BytesToUint32L(f[28:32]),
-    StrStart:   conv.BytesToUint32L(f[32:36]),
-    StyleStart: conv.BytesToUint32L(f[36:40]),
-  }
-
-  fmt.Printf("tableHeader:%+v\n", tableHeader)
-  fmt.Printf("strPoolHeader:%+v\n", strPoolHeader)
-
-  var strOffsets []uint32
-  if strPoolHeader.StrCount > 0 {
-    strOffsets = make([]uint32, strPoolHeader.StrCount)
-    var s, e uint32
-    i := uint32(0)
-    for ; i < strPoolHeader.StrCount; i++ {
-      s = 40 + i*4
-      e = s + 4
-      strOffsets[i] = conv.BytesToUint32L(f[s:e])
-    }
-  }
-
-  var styleOffsets []uint32
-  if strPoolHeader.StyleCount > 0 {
-    styleOffsets = make([]uint32, strPoolHeader.StyleCount)
-    var s, e uint32
-    i := uint32(0)
-    for ; i < strPoolHeader.StyleCount; i++ {
-      s = 40 + i*4
-      e = s + 4
-      styleOffsets[i] = conv.BytesToUint32L(f[s:e])
-    }
-  }
-
-  var strs []string
-  if strPoolHeader.StrCount > 0 {
-    strs = make([]string, strPoolHeader.StrCount)
-    i := uint32(0)
-    p := 12 + strPoolHeader.StrStart
-    for ; i < strPoolHeader.StrCount; i++ {
-      s := p + strOffsets[i] + 2
-      if i < strPoolHeader.StrCount-1 {
-        strs[i] = string(f[s : p+strOffsets[i+1]])
-      } else {
-        if strPoolHeader.StyleCount > 0 {
-          strs[i] = string(f[s : 12+strPoolHeader.StyleStart])
-        } else {
-          strs[i] = string(f[s : 12+strPoolHeader.Size])
-        }
-      }
-    }
-  }
-
-  strPoolChunk := ResStrPoolChunk{
-    Header:       strPoolHeader,
-    StrOffsets:   strOffsets,
-    StyleOffsets: styleOffsets,
-    Strs:         strs,
-    Styles:       nil,
-  }
-
-  for i := 100; i < 250; i++ {
-    fmt.Println(strPoolChunk.Strs[i])
+  arsc := ParseResArsc(file)
+  for i := 0; i < 10; i++ {
+    fmt.Println(arsc.GlobalStrPoolChunk.Strs[i])
   }
 }
