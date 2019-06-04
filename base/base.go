1d956a00ad6e9b0193a020e16d8dd58a62761460
WangFeng <kwf2030@163.com>
2019-06-04 16:22:23 +0800

    update

---
+++ base/base.go
@@ -0,0 +1,15 @@
+package base
+
+type Equality interface {
+  Equals(interface{}) bool
+
+  HashCode() int
+}
+
+type Comparable interface {
+  CompareTo(interface{}) int
+}
+
+type Cloneable interface {
+  Clone() interface{}
+}
